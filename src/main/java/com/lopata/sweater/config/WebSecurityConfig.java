11c111f3da021c7ac318a935679c8a8d0109ace2
Арт Дим <deadbox33@gmail.com>
2022-05-02 14:54:50 +0300

    DIMAS

--- src/main/java/com/lopata/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/lopata/sweater/config/WebSecurityConfig.java
@@ -10,6 +10,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
